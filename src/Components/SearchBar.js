8cacbbf120237a72f00bc843999d3825fd78990e
Niraj Parab <nirajparab007@gmail.com>
2024-03-02 13:48:56 +0530

    create SerachBar first draft

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <div className="w-screen">
+      <form className="mx-auto w-5/12 my-2 grid grid-cols-12 gap-2">
+        <input
+          className="px-2 py-2 border border-black rounded-md col-span-9"
+          type="text"
+          placeholder="What you will read today"
+        />
+        <button
+          className="border border-black col-span-3 rounded-md px-3"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
